96bd2cc048e5c8049c9988adcbc39118eced4de4
Sheneeb Sadhik <sadhik.sheneeb@gmx.com>
2018-10-12 13:36:10 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,8 +1,6 @@
 # code your #position_taken? method here!
 def position_taken(board, index)
-  if board[index] == " "
-    true
-  else
-    false
+  if board[index] != " " || board[index] != ""
+    
   end
 end
